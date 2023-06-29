bd9379d23ff823d71d216a344e20f416b6b6dbd5
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-06-29 15:19:26 +0200

    loop modified

--- Movies.java
+++ Movies.java
@@ -8,7 +8,7 @@ class Movies {
         };
 
         for (int i = 0; i < films.length; i++) {
-		System.out.println ("Dans le film " + films[i] + ", les principaux acteurs sont : " + actors[i][0] + ", " + actors[i][1] +  ", " + actors[i][2]);
+		System.out.println ("In the movie " + films[i] + ", the main actors are : " + actors[i][0] + ", " + actors[i][1] +  ", " + actors[i][2]);
 		}
     }
 }
